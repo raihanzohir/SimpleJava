2a49ee617b4e6cb2a4eb2fd74e22c6533c74e632
Md. Zohir Raihan <zohir@era.com.bd>
2019-04-10 15:05:10 +0600

    Modified App.java

--- src/main/java/com/era/simplejava/App.java
+++ src/main/java/com/era/simplejava/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is Simple Java!" );
+        System.out.println( "First Session on Basic Java" );
     }
 }
